cd5aaab1bb4c94909eea36d6c2f9ed66b2abb311
Paulo Ferreira <paulo93f@gmail.com>
2025-03-23 16:28:55 +0100

    Added create and edit blog functions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,4 +10,89 @@ class BlogsController < ApplicationController
       redirect_to blogs_path, alert: "Blog no encontrado"
     end
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    title = params[:blog][:title].presence || "Sin título"
+    content = params[:blog][:content]
+    slug = params[:blog][:slug].presence || title.parameterize
+
+    created_at = Time.current.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Creamos el frontmatter
+    frontmatter = {
+      'title' => title,
+      'created_at' => created_at,
+      'updated_at' => created_at,
+      'slug' => slug
+    }
+
+    # Creamos el contenido del archivo
+    file_content = "---\n#{frontmatter.to_yaml}---\n\n#{content}"
+
+    # Guardamos el archivo
+    file_path = Rails.root.join("content/blogs/#{slug}.md")
+
+    begin
+      File.write(file_path, file_content)
+      redirect_to blog_path(slug), notice: "Blog creado exitosamente"
+    rescue => e
+      flash.now[:alert] = "Error al guardar el blog: #{e.message}"
+      render :new
+    end
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+
+    if @blog.nil?
+      redirect_to blogs_path, alert: "Blog no encontrado"
+    end
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+
+    if @blog.nil?
+      redirect_to blogs_path, alert: "Blog no encontrado"
+      return
+    end
+
+    title = params[:blog][:title].presence || @blog.title
+    content = params[:blog][:content]
+    slug = params[:blog][:slug].presence || @blog.slug
+
+    # Actualizamos el frontmatter
+    frontmatter = {
+      'title' => title,
+      'created_at' => @blog.created_at.strftime('%Y-%m-%dT%H:%M:%SZ'),
+      'updated_at' => Time.current.strftime('%Y-%m-%dT%H:%M:%SZ'),
+      'slug' => slug
+    }
+
+    # Creamos el contenido del archivo
+    file_content = "---\n#{frontmatter.to_yaml}---\n\n#{content}"
+
+    # Actualizamos el archivo actual
+    current_file_path = Rails.root.join("content/blogs/#{@blog.slug}.md")
+    new_file_path = Rails.root.join("content/blogs/#{slug}.md")
+
+    begin
+      # Si cambió el slug, eliminar el archivo antiguo
+      if @blog.slug != slug && File.exist?(current_file_path)
+        File.delete(current_file_path)
+      end
+
+      # Escribir el nuevo archivo
+      File.write(new_file_path, file_content)
+
+      redirect_to blog_path(slug), notice: "Blog actualizado exitosamente"
+    rescue => e
+      flash.now[:alert] = "Error al actualizar el blog: #{e.message}"
+      render :edit
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # get "pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :blogs, only: [:index]
+  resources :blogs, only: [:index, :show, :new, :create, :edit, :update]
    # Opcional: hacer que /blog/:id también funcione
-  get 'blog/:id', to: 'blogs#show', as: 'blog'
+  # get 'blog/:id', to: 'blogs#show', as: 'blog'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
